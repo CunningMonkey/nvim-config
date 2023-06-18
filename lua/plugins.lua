c28507b2094b280a12cb1faa8fa88a43922c70fd
wukong <wukong86372@qq.com>
2023-06-18 13:21:34 +0800

    修复了一些图标的显示，修改了which-key的功能，添加了ufo，用来折叠代码

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -107,7 +107,7 @@ return packer.startup(function(use)
 		"folke/which-key.nvim",
 		config = function()
 			vim.o.timeout = true
-			vim.o.timeoutlen = 300
+			vim.o.timeoutlen = 1000
 			require("which-key").setup({
 				-- your configuration comes here
 				-- or leave it empty to use the default settings
@@ -117,6 +117,9 @@ return packer.startup(function(use)
 	})
 
 	use("ahmedkhalf/project.nvim")
+
+	--fold/unfold
+	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
 
 	-- debug DAP
 
